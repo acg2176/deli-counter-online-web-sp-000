e93e5daa14a4af1ebfa1e2dff7f5ad33182eb8a9
Anna Guerrero <acg2176@columbia.edu>
2020-03-25 19:34:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,35 @@
 # Write your code here.
+def line(line)
+  if line.length() == 0
+    puts "The line is currently empty."
+  else
+    new_string = ""
+    line.each_with_index do |person, index|
+      index += 1
+      person_i = " #{index}. #{person}"
+      new_string << person_i
+    end
+    puts "The line is currently:#{new_string}"
+    end
+end
+
+def take_a_number(line, person)
+  if line.length() == 0
+    index = 1
+    puts "Welcome, #{person}. You are number #{index} in line."
+    line << person
+
+  else
+    line << person #[L,A,S,G]
+    final = line.index(line[-1]) + 1
+    puts "Welcome, #{line[-1]}. You are number #{final} in line."
+  end
+
+end
+
+def now_serving(line)
+  if line.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+
+end
